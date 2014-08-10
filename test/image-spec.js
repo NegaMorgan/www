80576fa7a81e8f75b5f8a064256a7691bb7bcd0e
Morgan Evans <morganrevans@gmail.com>
2014-08-09 20:23:10 -0400

    created image module; organized all the things

---
+++ test/image-spec.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var _ = require('lodash');
+var path = require('path');
+var fs = require('fs');
+var http = require('http');
+var is = require('is-predicate');
+var should = require('chai').should();
+var expect = require('chai').expect;
+var image = require('../lib/image');
+
+describe('image', function(){
+  // set up a dummy image to download
+  var destination = path.join(__dirname, '/fixtures/kitten.jpg');
+  
+  after(function(){
+    // delete that dummy image added by test
+    fs.unlink(destination, function (err) {
+      if (err) throw err;
+    });
+  });
+  
+  describe('#saveFile', function(){
+    it('saves the file to the provided destination', function(){
+      // TODO figure out how to mock this http request
+      var file = 'http://placekitten.com/150/150';
+      image.saveFile(file, destination);
+
+      expect('destination').to.exist;
+    });
+    it('handles get request errors', function(){
+
+    });
+    it('handles write errors', function(){
+
+    });
+  });
+  describe('#append', function(){
+
+  });
+
+});
--- test/meetups-spec.js
+++ test/meetups-spec.js
@@ -3,38 +3,7 @@
 var _ = require('lodash');
 var is = require('is-predicate');
 var should = require('chai').should();
-var expect = require('chai').expect;
 var meetups = require('../lib/meetups');
-
-describe('profile image getter', function(){
-  var destination = path.join(__dirname, '/fixtures/kitten.jpg');
-  
-  after(function(){
-    // delete that kitten image added by test
-    fs.unlink(destination, function (err) {
-      if (err) throw err;
-    });
-  });
-  
-  describe('#saveFile', function(){
-    it('saves the file to the provided destination', function(){
-      var file = 'http://placekitten.com/150/150';
-      saveFile(file, destination);
-
-      expect('destination').to.exist;
-    });
-    it('handles get request errors', function(){
-
-    });
-    it('handles write errors', function(){
-
-    });
-  });
-  describe('#getPhoto', function(){
-
-  });
-
-});
 
 describe('meetups', function() {
   it('should be sorted by latest date', function() {
